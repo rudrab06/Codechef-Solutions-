02d5e6c77a63d79ce3e9c4b6b8b1616ed235e130
rudrab06 <rudrabaunk@gmail.com>
2025-01-17 10:34:03 +0530

    Saving Taxes solution
    it consists the solution of Saving Taxes codechef question difficulty-252

---
+++ Saving Taxes.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int X,Y;
+	    cin>>X>>Y;
+	    cout<<X-Y<<endl;
+	}
+	return 0;
+}
