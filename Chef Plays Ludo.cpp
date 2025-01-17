3d254cbed39c6b54cbd10a325c25369fb31d4462
rudrab06 <rudrabaunk@gmail.com>
2025-01-17 10:47:42 +0530

    Chef Plays Ludo solution
    uploaded the solution of Chef Plays Ludo codechef question difficulty -260

---
+++ Chef Plays Ludo.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n==6){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+
+}
