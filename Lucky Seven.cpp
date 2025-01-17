766c91068770c4b42df8029ee97f282d14cc0d86
rudrab06 <rudrabaunk@gmail.com>
2025-01-17 10:16:07 +0530

    Lucky Seven codechef solution
    it consists the solution of Lucky Seven question from codechef difficulty-213

---
+++ Lucky Seven.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	char arr[10];
+	for(int i=0;i<10;i++){
+	    cin>> arr[i];
+	}
+	cout<<arr[6]<<endl;
+	
+	return 0;
+	
+}
+
