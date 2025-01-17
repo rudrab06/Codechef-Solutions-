8851b1f7cc3338aea6fe1e3d072d50fbceabe397
rudrab06 <rudrabaunk@gmail.com>
2025-01-17 10:40:12 +0530

    Masterchef finals codechef solution
    uploaded the codechef solution of Masterchef finals question difficulty - 255

---
+++ Masterchef finals.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x<=10){
+	        cout<<"Yes";
+	    }
+	    else{
+	        cout<<"No";
+	    }
+	    cout<<"\n";
+	}
+	return 0;
+
+}
