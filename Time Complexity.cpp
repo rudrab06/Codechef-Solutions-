a789fd6d50f520371c628cec78e0fdd97ce4944b
rudrab06 <rudrabaunk@gmail.com>
2025-01-18 13:33:30 +0530

    Time Complexity Solution
    It consists the solution of Time Complexity codechef problem difficulty- 364

---
+++ Time Complexity.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    if(a>b){
+	        cout<<"Yes"<<endl;
+	    }
+	    else{
+	        cout<<"No"<<endl;
+	    }
+	}
+	return 0;
+
+}
