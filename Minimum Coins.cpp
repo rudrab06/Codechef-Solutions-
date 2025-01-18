42452ca7173814fb79522a1908308599b4a17c72
rudrab06 <rudrabaunk@gmail.com>
2025-01-18 13:46:57 +0530

    Minimum Coins Solution
    It consists the solution of Minimum Coins codechef problem difficulty -390

---
+++ Minimum Coins.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if (n<10){
+	       cout<<n<<endl; 
+	    }
+	    else{
+	        cout<<n%10<<endl;
+	    }
+	    
+}
+  return 0;
+}
