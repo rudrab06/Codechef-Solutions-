a3758e3e9d0594f7613afb578c2dec71c5648f0c
rudrab06 <rudrabaunk@gmail.com>
2025-01-18 12:59:43 +0530

    Four Tickets solution
    it consists the solution of Four Tickets solution of codechef problem difficulty- 302

---
+++ Four Tickets.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if (n*4 <= 1000){
+	        cout<<"Yes"<<endl;
+	    }
+	    else{
+	        cout<<"No"<<endl;
+	    }
+	}
+	return 0;
+
+}
