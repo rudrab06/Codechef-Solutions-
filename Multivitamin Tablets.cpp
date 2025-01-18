847bf1acebf3652156d2976dd6f409aadaf84aea
rudrab06 <rudrabaunk@gmail.com>
2025-01-18 13:40:15 +0530

    Multivitamin Tablets solution
    It consists the solution of Multivitamin Tablets codechef problem difficulty- 376

---
+++ Multivitamin Tablets.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    if(a*3<=b){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+
+}
