81dfb98b4618080cbf5a77f939be0ad7c0c5fd81
rudrab06 <rudrabaunk@gmail.com>
2025-01-19 12:51:08 +0530

    Ezio and Guards solution
    it consists the solution of Ezio and Guards codechef problem difficulty - 427

---
+++ Ezio and Guards.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    if(a-b>=0){
+	    cout<<"Yes"<<endl;}
+	    else{
+	        cout<<"No"<<endl;
+	    }
+	}
+	return 0;
+
+}
