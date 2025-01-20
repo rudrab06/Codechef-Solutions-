6949012a64f8a735472af2355463bb8c8f81dbc2
rudrab06 <rudrabaunk@gmail.com>
2025-01-20 14:31:55 +0530

    Chef in his Office Solution
    It consists the solution of Chef in his Office codechef problem -532

---
+++ Chef in his Office.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    cout<<4*a+b<<endl;
+	}
+	return 0;
+
+}
