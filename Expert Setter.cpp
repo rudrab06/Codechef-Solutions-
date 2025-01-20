88a807818c3af2d9380e1ff6a409d85fd2e9be44
rudrab06 <rudrabaunk@gmail.com>
2025-01-20 14:54:07 +0530

    Expert Setter solution
    It consists the solution of Expert Setter codechef problem difficulty - 561

---
+++ Expert Setter.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    float x,y;
+	    cin>>x>>y;
+	    if(x/2 <= y){
+	        cout<<"Yes"<<endl;
+	    }
+	    else{
+	        cout<<"No"<<endl;
+	    }
+	}
+	return 0;
+
+}
