a87f8dff3b5ef36a7d9fb4a64a7410727679e3f7
rudrab06 <rudrabaunk@gmail.com>
2025-01-21 12:53:01 +0530

    Maximise the Tastiness Solution
    It consists the solution of Maximise the Tastiness codechef problem difficulty-627

---
+++ Maximise the Tastiness.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c,d;
+	    cin>>a>>b>>c>>d;
+	    if(a>b){
+	        if(c>d){
+	            cout<<a+c<<endl;
+	        }
+	        else{
+	            cout<<a+d<<endl;
+	        }
+	    }
+	    else{
+	        if(c>d){
+	            cout<<b+c<<endl;
+	        }
+	        else{
+	            cout<<b+d<<endl;
+	        }
+	    }
+	}
+	return 0;
+
+}
