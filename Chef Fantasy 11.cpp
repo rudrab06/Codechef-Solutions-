3732014d4badadc7948ecedb4b7f76eb27e1658e
rudrab06 <rudrabaunk@gmail.com>
2025-01-27 15:48:26 +0530

    Chef Fantasy 11 Solution
    It consists the Solution of Chef Fantasy 11 codechef problem difficulty-739

---
+++ Chef Fantasy 11.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    cout<<(n-1)*n<<endl;
+}
+return 0; }
